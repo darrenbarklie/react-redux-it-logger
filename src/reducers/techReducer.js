9f2982009b115ca89f6f16efd59165955b574ea2
Darren Barklie <darrenbarklie@gmail.com>
2019-11-12 09:13:16 +0000

    Techs Reducer, Action and Component

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,25 @@
+import { SET_LOADING, GET_TECHS } from '../actions/types'
+
+const initialState = {
+  techs: null,
+  loading: false,
+  error: null,
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      }
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      }
+    default:
+      return state
+  }
+}
